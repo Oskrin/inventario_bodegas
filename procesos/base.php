103033f4a9a507928b802d380571c342aa016157
Oskrin <oskrs11@hotmail.com>
2014-12-03 10:40:25 -0500

    er
    ewr

--- procesos/base.php
+++ procesos/base.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarse() {
-    if (!($conexion = pg_pconnect("host=localhost port=5432 dbname=inventario_bodegas user=postgres password=root"))) {
+    if (!($conexion = pg_pconnect("host=localhost port=5432 dbname=inventario_bodegas user=postgres password=rootdow"))) {
         exit();
     }
     return $conexion;
